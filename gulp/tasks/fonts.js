2eccb8ebdb8954c54b8e60dd25b755244c17a9f7
herold89 <heroldserg89@gmail.com>
2024-04-25 22:08:14 +0300

    Исправление gulp fonts

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -4,19 +4,25 @@ import ttf2woff2 from 'gulp-ttf2woff2';
 import config from '../config.js';
 
 const ttf2woffBuild = () => (
-  gulp.src(`${config.src.fonts}/**/*`)
+  gulp.src(`${config.src.fonts}/*.ttf`)
     .pipe(ttf2woff())
-    .pipe(gulp.dest(config.dest.fonts))
+    .pipe(gulp.dest(config.src.fonts))
 );
 
 const ttf2woff2Build = () => (
-  gulp.src(`${config.src.fonts}/**/*`)
+  gulp.src(`${config.src.fonts}/*.ttf`)
     .pipe(ttf2woff2())
+    .pipe(gulp.dest(config.src.fonts))
+);
+
+export const ttfFontsBuild = gulp.parallel(ttf2woffBuild, ttf2woff2Build);
+
+export const fontsBuild = () => (
+  gulp.src(`${config.src.fonts}/*.{woff,woff2}`)
     .pipe(gulp.dest(config.dest.fonts))
 );
 
-export const fontsBuild = gulp.parallel(ttf2woffBuild, ttf2woff2Build);
-
-export const fontsWatch = () => {
-  gulp.watch(`${config.src.fonts}/**/*`, fontsBuild);
+export const ttfFontsWatch = () => {
+  gulp.watch(`${config.src.fonts}/*.ttf`, ttfFontsBuild);
+  gulp.watch(`${config.src.fonts}/*.{woff,woff2}`, fontsBuild);
 };
