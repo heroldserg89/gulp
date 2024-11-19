8280892393315cfe0541f2213e47225f4819b8ad
´Herold89´ <heroldserg89@gmail.com>
2024-11-19 10:55:30 +0300

    Изменение pugBuild

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,44 +1,16 @@
 import gulp from 'gulp';
-import pug from 'gulp-pug';
-import gulpif from 'gulp-if';
 import plumber from 'gulp-plumber';
-import { setup as emittySetup } from '@zoxon/emitty';
+import pug from 'gulp-pug';
+import cached from 'gulp-cached';
 import config from '../config.js';
 
-const emittyPug = emittySetup(config.src.pug, 'pug', {
-  makeVinylFile: true,
-});
-
-global.isPugWatch = false;
-global.emittyChangedFile = {
-  path: '',
-  stats: null,
-};
-
-export const pugBuild = () => (
-  gulp.src(`${config.src.pug}/*.pug`)
-    .pipe(plumber())
-    .pipe(
-      gulpif(
-        global.isPugWatch,
-        emittyPug.stream(
-          global.emittyChangedFile.path,
-          global.emittyChangedFile.stats,
-        ),
-      ),
-    )
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest(config.dest.html))
-);
+export const pugBuild = () => gulp
+  .src(`${config.src.pug}/*.pug`)
+  .pipe(plumber())
+  .pipe(pug({ pretty: true }))
+  .pipe(cached('pug'))
+  .pipe(gulp.dest('build'));
 
 export const pugWatch = () => {
-  global.isPugWatch = true;
-
-  gulp.watch(`${config.src.pug}/**/*.pug`, pugBuild)
-    .on('all', (event, filepath, stats) => {
-      global.emittyChangedFile = {
-        path: filepath,
-        stats,
-      };
-    });
+  gulp.watch(`${config.src.pug}/**/*.pug`, pugBuild);
 };
